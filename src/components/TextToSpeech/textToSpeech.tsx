7c75eda92610991eb8e3c3ed3f8ac4923a2617b9
Kiran Ponnam <kiran@CALAP-55.craftany.com>
2022-06-20 11:26:16 +0530

    changes

--- src/components/TextToSpeech/textToSpeech.tsx
+++ src/components/TextToSpeech/textToSpeech.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
 import { Button } from "@mui/material";
 import "./textToSpeech.css";
-import { Campaign } from "@mui/icons-material";
+
 const TextToSpeech = (pros: any) => {
   const [text, setText] = useState<string>("");
   const textHandler = (e: any) => {
@@ -14,6 +14,35 @@ const TextToSpeech = (pros: any) => {
     utter.text = text;
     window.speechSynthesis.speak(utter);
   };
+  function populateVoiceList() {
+    if (typeof speechSynthesis === "undefined") {
+      return;
+    }
+
+    var voices = speechSynthesis.getVoices();
+
+    for (var i = 0; i < voices.length; i++) {
+      var option = document.createElement("option");
+      option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+
+      if (voices[i].default) {
+        option.textContent += "-- DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      document?.getElementById("voiceSelect")?.appendChild(option);
+    }
+  }
+  useEffect(() => {
+    populateVoiceList();
+  }, []);
+  if (
+    typeof speechSynthesis !== "undefined" &&
+    speechSynthesis.onvoiceschanged !== undefined
+  ) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
   return (
     <React.Fragment>
       <br />
@@ -22,47 +51,49 @@ const TextToSpeech = (pros: any) => {
       </header>
       <br />
       <div className="main-text-container">
+        <b>Select Voice</b>
+        <select id="voiceSelect" className="voice-select-container"></select>
         <TextareaAutosize
           onChange={(e: any) => textHandler(e)}
           minRows={4}
           placeholder="Enter your text"
           className="input-text"
-          style={{width: "341px"}}
+          style={{ width: "341px" }}
         />
-        <br/>
+        <br />
         <div>
-        <Button
-          onClick={() => speakHandler()}
-          variant="contained"
-          style={{margin:'2px'}}
-          color="success"
-        >
-          Speak
-        </Button>
-        <Button
-          onClick={() => window.speechSynthesis.pause()}
-          variant="contained"
-          style={{margin:'2px'}}
-          color="warning"
-        >
-          Pause
-        </Button>
-        <Button
-          onClick={() => window.speechSynthesis.resume()}
-          variant="contained"
-          style={{margin:'2px'}}
-          color="info"
-        >
-          Resume
-        </Button>
-        <Button
-          onClick={() => window.speechSynthesis.cancel()}
-          variant="contained"
-          style={{margin:'2px'}}
-          color="error"
-        >
-          Cancel
-        </Button>
+          <Button
+            onClick={() => speakHandler()}
+            variant="contained"
+            style={{ margin: "2px" }}
+            color="success"
+          >
+            Speak
+          </Button>
+          <Button
+            onClick={() => window.speechSynthesis.pause()}
+            variant="contained"
+            style={{ margin: "2px" }}
+            color="warning"
+          >
+            Pause
+          </Button>
+          <Button
+            onClick={() => window.speechSynthesis.resume()}
+            variant="contained"
+            style={{ margin: "2px" }}
+            color="info"
+          >
+            Resume
+          </Button>
+          <Button
+            onClick={() => window.speechSynthesis.cancel()}
+            variant="contained"
+            style={{ margin: "2px" }}
+            color="error"
+          >
+            Cancel
+          </Button>
         </div>
       </div>
     </React.Fragment>
